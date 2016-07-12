9e4be430c4a2d1ebe6922e4926d9b7568d14edd6
Jan Benscheid <jan.benscheid@gmail.com>
2016-07-13 00:12:21 +0200

    small correction in string hashing

--- src/js/util.js
+++ src/js/util.js
@@ -3,8 +3,8 @@ util = {
     var hash = 0;
     if (string.length == 0) return hash;
     for (i = 0; i < string.length; i++) {
-      char = string.charCodeAt(i);
-      hash = ((hash<<5)-hash)+char;
+      cha = string.charCodeAt(i);
+      hash = ((hash<<5)-hash)+cha;
       hash = hash & hash; // Convert to 32bit integer
     }
     return Math.abs(hash);
