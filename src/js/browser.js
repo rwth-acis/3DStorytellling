8d54e740d7181a09af2072f518418dd738bff777
Jan Benscheid <jan.benscheid@gmail.com>
2016-07-31 00:55:46 +0200

    story persistence scatterfold

--- src/js/browser.js
+++ src/js/browser.js
@@ -8,29 +8,38 @@ browser.init = function (eM) {
         $linked = $('#link_dialog'),
         $redirector = $linked.find('#space_redirector'),
         $addModel = $('#plus_button_models'),
-        $addModelDialoge = $('#add_model'),
+        $addModelDialog = $('#add_model'),
+        $editStoryDialog = $('#edit_story'),
         $modelForm = $('#model_form'),
         $modelSubmit = $('#model_submit'),
+        $storySubmit = $('#story_submit'),
         $modelsAjax = $('#models_ajax'),
         $sureDialog = $('#sure_dialog'),
         $sureButton = $('#sure_button'),
 
         editorMode = eM,
         selectedModel = null,
+        selectedStory = null,
         context = null,
+        onEditModelClose = null,
         iwcClient = new Las2peerWidgetLibrary(conf.external.LAS,
                                               function () {}, "ATTRIBUTE");
 
         _init = function () {
           window.y = y;
 
-          $addModel.on('click', openAddModelDialoge);
+          $addModel.on('click', function (e) {
+            context = 'addModel';
+            openAddModelDialog(e, null);
+          });
+          $addStory.on('click', addStoryButtonClick);
           $modelForm.submit(previewModel);
           $modelSubmit.on('click', submitModel);
+          $storySubmit.on('click', submitStory);
           $sureButton.on('click', sure);
-          $elem.addEventListener('deleteModel', openDeleteModelDialoge);
+          $elem.addEventListener('deleteModel', openDeleteModelDialog);
           $elem.addEventListener('loadStory', loadStory);
-          $elem.addEventListener('editStory', editStory);
+          $elem.addEventListener('editStory', editStoryClick);
           $elem.addEventListener('loadModel', loadModel);
           $elem.addEventListener('editModel', editModel);
         };
@@ -53,21 +62,33 @@ browser.init = function (eM) {
     };
 
     var submitModel = function (e) {
-      var values = util.serializeForm($modelForm);
 
-      iwcClient.sendRequest('post', '3DST/models', JSON.stringify(values),
-                            'application/json',
-                            function (data, type) {
-                              $addModelDialoge[0].close();
-                              $modelsAjax[0].generateRequest();
-                            },
-                            function (error) {
-                              console.log(error);
-                            });
+      var values = util.serializeForm($modelForm);
+      if (context == 'addModel') {
+        iwcClient.sendRequest('post', '3DST/models', JSON.stringify(values),
+                              'application/json',
+                              function (data, type) {
+                                $addModelDialog[0].close();
+                                $modelsAjax[0].generateRequest();
+                              },
+                              function (error) {
+                                console.log(error);
+                              });        
+      } else if (context == 'editModel') {
+        iwcClient.sendRequest('put', '3DST/models/'+selectedModel, JSON.stringify(values),
+                              'application/json',
+                              function (data, type) {
+                                $addModelDialog[0].close();
+                                $modelsAjax[0].generateRequest();
+                              },
+                              function (error) {
+                                console.log(error);
+                              });
+
+      }
 
       return false;
     };
-
 
     var deleteModel = function (id) {
 
@@ -87,34 +108,90 @@ browser.init = function (eM) {
       return deferred.promise();
     };
 
-    var openDeleteModelDialoge = function (e) {
+    var openDeleteModelDialog = function (e) {
       selectedModel = e.detail.id;
       context = 'deleteModel';
       $sureDialog[0].open();
     };
 
-    var openAddModelDialoge = function (e) {
-      console.log('click');
-      $addModelDialoge[0].open();
+    var openAddModelDialog = function (e, d) {
+      var data = d || {name:"",url:"",description:""};
+      $addModelDialog.find('[name="modelName"]').val(data.name);
+      $addModelDialog.find('[name="modelURL"]').val(data.url);
+      $addModelDialog.find('[name="modelDescription"]').val(data.description);
+      
+      $addModelDialog[0].open();
     };
 
-    var loadStory = function (e) {
-      appendStory(false, e.detail.id);
-    };
-    
-    var editStory = function (e) {
-      appendStory(true, e.detail.id);
-    };
-    
     var loadModel = function (e) {
       window.y.share.data.set('model3d', e.detail.url);
     };
     
     var editModel = function (e) {
-      appendStory(true, e.detail.id);
+      context = 'editModel';
+      selectedModel = e.detail.data.id;
+      openAddModelDialog(e, e.detail.data);
     };
 
+    ///////////////////////////////////////////////////////////////
+    // Story Related
+    ///////////////////////////////////////////////////////////////
+
+    var submitStory = function (e) {
+      $editStoryDialog[0].close();
+      onEditModelClose();
+    };
+
+    var addStoryButtonClick = function (e) {
+      
+      if (editorMode) {
+        onEditModelClose = function () {
+          console.log('TODO: PERSISTENCE');
+        };
+      } else {
+        onEditModelClose = function () {
+          console.log('TODO: PERSISTENCE');
+          $redirector.attr('href', conf.external.ROLE + 'spaces/' +
+                           conf.external.editor);
+          $linked[0].open();
+        };
+      }
+      var data = e.detail.data;
+      $editStoryDialog.find('[name="storyName"]').val('');
+      $editStoryDialog.find('[name="storyDescription"]').val('');
+      $editStoryDialog[0].open();
+      
+    };
+    
+    var loadStory = function (e) {
+      appendStory(false, e.detail.id);
+    };
+    
+    var editStoryClick = function (e) {
+
+      if (editorMode) {
+        onEditModelClose = function () {
+          console.log('TODO: PERSISTENCE');
+        };
+        var data = e.detail.data;
+        $editStoryDialog.find('[name="storyName"]').val(data.name);
+        $editStoryDialog.find('[name="storyDescription"]')
+          .val(data.description);
+        $editStoryDialog[0].open();
+      } else {
+        appendStory(true, e.detail.id)
+          .then(function (sameSpace) {
+            if (sameSpace) {
+              $loaded[0].open();
+            } else {
+              $linked[0].open();
+            }
+          });
+      }
+    };
+    
     var appendStory = function (toEditor, id) {
+      var deferred = $.Deferred();
       var path = conf.external.STORY_ROOT+'story_'+id+'.json'; 
       $.getJSON(path, function (res) {
         if (editorMode != toEditor) {
@@ -122,13 +199,15 @@ browser.init = function (eM) {
           crossLoad(space, res)
             .then(function () {
               $redirector.attr('href', conf.external.ROLE + 'spaces/' + space);
-              $linked[0].open();
+              deferred.resolve(false);
             });
         } else {
           window.y.share.data.set('model', res);
-          $loaded[0].open();
+          deferred.resolve(true);
         }
       });
+
+      return deferred;
     };
 
     var crossLoad = function (room, model) {
