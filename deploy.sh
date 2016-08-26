4b2313899b08e78e59872a17044ae7116c851ea5
Jan Benscheid <jan.benscheid@gmail.com>
2016-08-26 13:31:13 +0200

    added simple deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,10 @@ echo "ROOT:"
 read ROOT
 
 OLD_ROOT="http://localhost:8082/"
-grep -rl $OLD_ROOT src | xargs sed -i s@$OLD_ROOT@$ROOT@g
+
+rm -rf html
+mkdir html
+cp -r src html/
+cp -r img html/
+
+grep -rl $OLD_ROOT html/src | xargs sed -i s@$OLD_ROOT@$ROOT@g
